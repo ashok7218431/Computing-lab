52c2dc9b4dd97dc1c50f0b78a69d225bdf5a5f2e
AshokPriyadarshi <44141084+ashok7218431@users.noreply.github.com>
2020-02-05 19:44:16 +0530

    Deletet the records Please enter the commit message for your changes. Lines starting

--- Assgn1/Assgn1_ques6.m
+++
@@ -1,6 +0,0 @@
-n=input('Enter the value of n \n');
-x=1;
-for i=1:n
-    x=x*(1+2/i);
-end
-fprintf('Product is %0.4f\n',x);
--- Assgn1/Assgn1_ques9.m
+++
@@ -1,5 +0,0 @@
-x=1;
-while (x*x*x<2000)
-    fprintf('%0.1f    %0.1f\n',x,(x*x*x));
-    x=x+1;
-end
